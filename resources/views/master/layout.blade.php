f5a304e7f51c97ac1321e96af935dfe15e347659
fatnin14 <fatninizzati.sidik@live.iium.edu.my>
2025-01-12 12:36:08 +0800

    layout updated

--- resources/views/master/layout.blade.php
+++ resources/views/master/layout.blade.php
@@ -61,7 +61,7 @@
                                         {{-- <li><a href="index.html">Home</a></li> --}}
                                         <li><a href="/" class="active">Home<br></a></li>
                                         <li><a href="">Patient</a></li>
-                                        <li><a href="">Appointment</a></li>
+                                        <li><a href="{{ route('appointment') }}">Appointment</a></li>
                                         {{-- <li><a href="{{ route('create-invoice') }}">Billing</a></li> --}}
                                         <li class="nav-item dropdown">
                                             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
