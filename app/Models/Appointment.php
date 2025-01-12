701ab41e58be0502469fd3caa5725a4cdec171d9
fatnin14 <fatninizzati.sidik@live.iium.edu.my>
2025-01-12 12:50:22 +0800

    appointment model update

--- app/Models/Appointment.php
+++ app/Models/Appointment.php
@@ -2,9 +2,19 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Appointment extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'appointment_id',
+        'patient_id',
+        'doctor_id',
+        'appointment_date',
+        'appointment_time',
+
+    ];
 }
