23b1ec0958554425a3779b3be35a7189ae2d5c2f
fatnin14 <fatninizzati.sidik@live.iium.edu.my>
2025-01-12 12:34:14 +0800

    appointment controller updated

--- app/Http/Controllers/AppointmentController.php
+++ app/Http/Controllers/AppointmentController.php
@@ -2,9 +2,39 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Appointment;
 use Illuminate\Http\Request;
 
 class AppointmentController extends Controller
 {
-    //
+    public function index()
+    {
+        $appointment = Appointment::all(); // Retrieve all appointment
+        return view('appointment', compact('appointments')); // Pass data to the view
+    }
+
+    public function store(Request $request)
+    {
+        $appointment = appointment::create($request->all());
+        return response()->json($appointment, 201);
+    }
+
+    public function show($id)
+    {
+        return Appointment::findOrFail($id);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $appointment = Appointment::findOrFail($id);
+        $appointment->update($request->all());
+        return response()->json($appointment, 200);
+    }
+
+    public function destroy($id)
+    {
+        Appointment::destroy($id);
+        return response()->json(null, 204);
+    }
 }
+
