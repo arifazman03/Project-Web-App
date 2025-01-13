041fb0beaa321f72217b60b64d38fa8d997731f5
arifazman03 <arif.azman@live.iium.edu.my>
2025-01-13 22:35:57 +0800

    Delete sushi

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,8 +16,8 @@
                                 <h1 class="cd-headline letters scale">We care about your
                                     <strong class="cd-words-wrapper">
                                         <b class="is-visible">health</b>
-                                        <b>sushi</b>
-                                        <b>steak</b>
+                                        {{-- <b>sushi</b>
+                                        <b>steak</b> --}}
                                     </strong>
                                 </h1>
                                 <p data-animation="fadeInLeft" data-delay="0.1s">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi uquip ex ea commodo consequat is aute irure.</p>
