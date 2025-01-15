ed3f262d354dcaf56b1dbcb04d75cd92ba444c35
fatnin14 <fatninizzati.sidik@live.iium.edu.my>
2025-01-15 17:25:00 +0800

    save

--- routes/web.php
+++ routes/web.php
@@ -47,10 +47,6 @@ Route::put('/patients/{id}', [PatientController::class, 'update'])->name('patien
 Route::delete('/patients/{id}', [PatientController::class, 'destroy'])->name('patients.destroy');
 
 
-
-
-
-
 Route::get('/appointments', [AppointmentController::class, 'index'])->name('appointments.index');
 Route::get('/appointments/create-appointment', [AppointmentController::class, 'create'])->name('appointments.create');
 Route::post('/appointments', [AppointmentController::class, 'store'])->name('appointments.store');
