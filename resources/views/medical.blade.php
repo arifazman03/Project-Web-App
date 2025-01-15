7e6b9a1bc1c278ca370eb1f666a9e5a37b4cbad8
adlynadika <adlynsofeawork@gmail.com>
2025-01-15 19:44:31 +0800

    edit all dept in medical

--- resources/views/medical.blade.php
+++ resources/views/medical.blade.php
@@ -39,13 +39,13 @@
                         <li class="nav-item">
                             <a class="nav-link" id="Astrology-tab" data-toggle="tab" href="#Astrology" role="tab" aria-controls="contact" aria-selected="false">
                                 <i class="flaticon-bone"></i>
-                                <h4>Astrology</h4>
+                                <h4>Orthopaedic</h4>
                             </a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" id="Neuroanatomy-tab" data-toggle="tab" href="#Neuroanatomy" role="tab" aria-controls="contact" aria-selected="false">
                                 <i class="flaticon-lung"></i>
-                                <h4>Neuroanatomy</h4>
+                                <h4>Pulmonology</h4>
                             </a>
                         </li>
                         <li class="nav-item">
@@ -64,15 +64,16 @@
                     <!-- single_content  -->
                     <div class="row align-items-center no-gutters">
                         <div class="col-lg-7">
+                            <!-- Dentistry -->
                             <div class="dept_info">
-                                <h3>Dentist with surgical mask holding <br> scaler near patient</h3 >
+                                <h3>Dentistry Department</h3 >
                                 <p>Our Dentistry Department is dedicated to providing comprehensive oral health care for patients of all ages. We strive to deliver the highest standards of dental care, helping you achieve a healthy and confident smile. </p>
                                 <a href="{{ route('medical.view_more') }}" class="dep-btn">View Records<i class="ti-arrow-right"></i></a>
                             </div>
                         </div>
                         <div class="col-lg-5">
                             <div class="dept_thumb">
-                                <img src="assets/img/gallery/department_man.png" alt="">
+                                <img src="assets/img/gallery/dentistry1.jpg" alt="">
                             </div>
                         </div>
                     </div>
@@ -82,15 +83,16 @@
                     <!-- single_content  -->
                     <div class="row align-items-center no-gutters">
                         <div class="col-lg-7">
+                            <!-- Cardiology -->
                             <div class="dept_info">
-                                <h3>Dentist with surgical mask holding <br> scaler near patient</h3 >
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. </p>
-                                <a href="#" class="dep-btn">Appointment<i class="ti-arrow-right"></i></a>
+                                <h3>Cardiology Department</h3 >
+                                <p>Our Cardiology Department is committed to providing exceptional heart care. We specialize in diagnosing and treating cardiovascular conditions, working with you to ensure a healthy heart and improve your overall well-being. Trust us to support your journey to a healthier heart. </p>
+                                <a href="#" class="dep-btn">View Records<i class="ti-arrow-right"></i></a>
                             </div>
                         </div>
                         <div class="col-lg-5">
                             <div class="dept_thumb">
-                                <img src="assets/img/gallery/department_man.png" alt="">
+                                <img src="assets/img/gallery/cardiology1.jpg" alt="" width="500" height="300">
                             </div>
                         </div>
                     </div>
@@ -100,15 +102,16 @@
                     <!-- single_content  -->
                     <div class="row align-items-center no-gutters">
                         <div class="col-lg-7">
+                            <!-- ENT -->
                             <div class="dept_info">
-                                <h3>Dentist with surgical mask holding <br> scaler near patient</h3 >
-                                <p>Our Dentistry Department is dedicated to providing comprehensive oral health care for patients of all ages. From routine check-ups and cleanings to advanced procedures like restorative and cosmetic dentistry, our skilled team ensures a comfortable and personalized experience. Equipped with state-of-the-art technology, we strive to deliver the highest standards of dental care, helping you achieve a healthy and confident smile. </p>
-                                <a href="#" class="dep-btn">Appointment<i class="ti-arrow-right"></i></a>
+                                <h3>ENT Specialist Department</h3 >
+                                <p>Our ENT Specialists are here to diagnose and treat conditions affecting the ear, nose, and throat. Whether it's hearing loss, sinus issues, or throat problems, we provide advanced care to ensure you breathe, hear, and speak with ease, helping you live a more comfortable life.</p>
+                                <a href="#" class="dep-btn">View Records<i class="ti-arrow-right"></i></a>
                             </div>
                         </div>
                         <div class="col-lg-5">
                             <div class="dept_thumb">
-                                <img src="assets/img/gallery/department_man.png" alt="">
+                                <img src="assets/img/gallery/ent1.jpg" alt="">
                             </div>
                         </div>
                     </div>
@@ -118,15 +121,16 @@
                     <!-- single_content  -->
                     <div class="row align-items-center no-gutters">
                         <div class="col-lg-7">
+                            <!-- Orthopaedic -->
                             <div class="dept_info">
-                                <h3>Dentist with surgical mask holding <br> scaler near patient</h3 >
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. </p>
-                                <a href="#" class="dep-btn">Appointment<i class="ti-arrow-right"></i></a>
+                                <h3>Orthopaedic Department</h3 >
+                                <p>Our Orthopaedic Department specializes in the diagnosis, treatment, and rehabilitation of musculoskeletal conditions. Whether you're dealing with fractures, joint pain, or sports injuries, our team of experts is dedicated to improving your mobility and helping you live a pain-free, active life.</p>
+                                <a href="#" class="dep-btn">View Records<i class="ti-arrow-right"></i></a>
                             </div>
                         </div>
                         <div class="col-lg-5">
                             <div class="dept_thumb">
-                                <img src="assets/img/gallery/department_man.png" alt="">
+                                <img src="assets/img/gallery/orto1.jpg" alt="">
                             </div>
                         </div>
                     </div>
@@ -136,15 +140,16 @@
                     <!-- single_content  -->
                     <div class="row align-items-center no-gutters">
                         <div class="col-lg-7">
+                            <!-- Pulmonology -->
                             <div class="dept_info">
-                                <h3>Dentist with surgical mask holding <br> scaler near patient</h3 >
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. </p>
-                                <a href="#" class="dep-btn">Appointment<i class="ti-arrow-right"></i></a>
+                                <h3>Pulmonology Department</h3 >
+                                <p>Our Pulmonology Department is dedicated to diagnosing and treating respiratory conditions. From asthma and COPD to lung infections and sleep apnea, our specialists provide comprehensive care to help you breathe easier and improve your lung health for a better quality of life.</p>
+                                <a href="#" class="dep-btn">View Records<i class="ti-arrow-right"></i></a>
                             </div>
                         </div>
                         <div class="col-lg-5">
                             <div class="dept_thumb">
-                                <img src="assets/img/gallery/department_man.png" alt="">
+                                <img src="assets/img/gallery/pulmo1.jpg" alt="">
                             </div>
                         </div>
                     </div>
@@ -154,15 +159,16 @@
                     <!-- single_content  -->
                     <div class="row align-items-center no-gutters">
                         <div class="col-lg-7">
+                            <!-- Blood Screening -->
                             <div class="dept_info">
-                                <h3>Dentist with surgical mask holding <br> scaler near patient</h3 >
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. </p>
-                                <a href="#" class="dep-btn">Appointment<i class="ti-arrow-right"></i></a>
+                                <h3>Blood Screening Department</h3 >
+                                <p>Our Blood Screening services provide comprehensive testing to monitor your health. With advanced diagnostic technology, we help detect early signs of diseases and conditions, offering you the information you need to maintain your health and wellness.</p>
+                                <a href="#" class="dep-btn">View Records<i class="ti-arrow-right"></i></a>
                             </div>
                         </div>
                         <div class="col-lg-5">
                             <div class="dept_thumb">
-                                <img src="assets/img/gallery/department_man.png" alt="">
+                                <img src="assets/img/gallery/blood1.jpg" alt="">
                             </div>
                         </div>
                     </div>
