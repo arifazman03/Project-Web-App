de3cf0172c0b4a8c29ee967632e9af982d5e2efa
fatnin14 <fatninizzati.sidik@live.iium.edu.my>
2025-01-15 14:33:00 +0800

    save

--- resources/views/edit-appointment.blade.php
+++ resources/views/edit-appointment.blade.php
@@ -24,8 +24,8 @@
             <div class="row justify-content-center">
                 <div class="col-lg-6">
                     <div class="section-tittle text-center mb-100">
-                        <span>Book an Appointment</span>
-                        <h2>Schedule Your Visit</h2>
+                        <span>Appointment</span>
+                        <h2>Book Appointment</h2>
                     </div>
                 </div>
             </div>
